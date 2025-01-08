f582d6df6c7f01ef5fa30b9232db389cbb1da65e
kinomagnum <kinomagnum@gmail.com>
2025-01-08 19:52:14 +0300

    initial commit3

--- src/main/java/polymorphism/Animal.java
+++ src/main/java/polymorphism/Animal.java
@@ -2,6 +2,34 @@ package polymorphism;
 
 public abstract class Animal {
 
+    protected String name;
+    protected String color;
+    protected int age;
+    protected double weight;
+
+    public Animal(String name, String color, int age, double weight) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
     public void makeSound(){
     }
 }
